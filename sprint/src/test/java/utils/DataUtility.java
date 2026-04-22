e24ad75a4f686ed328163bc24d35438b199705da
Rajmohan <rsat338@gmail.com>
2026-04-22 08:30:06 +0530

    Added Required Post Module  Methods to DataUtility and Assertions Files.....

--- sprint/src/test/java/utils/DataUtility.java
+++ sprint/src/test/java/utils/DataUtility.java
@@ -1,5 +1,19 @@
 package utils;
 
+import java.util.Map;
+
 public class DataUtility {
-    
+
+    public static String buildPostJson(String title, String body, int userId) {
+        return String.format(
+                "{\"user_id\":%d,\"title\":\"%s\",\"body\":\"%s\"}",
+                userId, title, body);
+    }
+
+    public static String buildPostJson(Map<String, String> row) {
+        return buildPostJson(
+                row.get("title"),
+                row.get("body"),
+                Integer.parseInt(row.get("user_id")));
+    }
 }
