f4213846487614f07648aa4f3eea4d9c441d1414
Rajmohan <rsat338@gmail.com>
2026-04-27 08:32:43 +0530

    Updated TestRunner.java...

--- sprint/src/test/java/runner/TestRunner.java
+++ sprint/src/test/java/runner/TestRunner.java
@@ -3,15 +3,10 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-		features = "src/test/resources/features",
-		glue = {"stepDefinitions", "hooks"},
-		plugin = {
-							"pretty",
-							"html:target/report.html"
-		},
-		monochrome = true
-)
-public class TestRunner extends AbstractTestNGCucumberTests{
-
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinitions", "hooks" }, plugin = {
+		"pretty",
+		"html:target/report.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+}, monochrome = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
