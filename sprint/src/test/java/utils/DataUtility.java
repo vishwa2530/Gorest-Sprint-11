3cbf74f233778bde0033374d1d3e1fb5ab7be106
Kameshm7 <kameshmofficial@gmail.com>
2026-04-27 09:16:53 +0530

    Updated Todos Datautility file

--- sprint/src/test/java/utils/DataUtility.java
+++ sprint/src/test/java/utils/DataUtility.java
@@ -1,5 +1,31 @@
+
+
 package utils;
 
 public class DataUtility {
-    
+    public static String buildTodoJson(String title, String status, int userId) {
+
+        return "{ \"user_id\": " + userId +
+               ", \"title\": \"" + title +
+               "\", \"status\": \"" + status + "\" }";
+    }
+    public static String buildTodoWithInvalidUser(String title, String status, int userId) {
+
+        return "{ \"user_id\": " + userId +
+               ", \"title\": \"" + title +
+               "\", \"status\": \"" + status + "\" }";
+    }
+    public static String emptyPayload() {
+        return "{}";
+    }
+    public static String updateTodoJson(String title, String status, int userId) {
+
+        return "{ \"user_id\": " + userId +
+               ", \"title\": \"" + title +
+               "\", \"status\": \"" + status + "\" }";
+    }
+    public static String patchTodoJson(String title) {
+
+        return "{ \"title\": \"" + title + "\" }";
+    }
 }
